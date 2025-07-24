691469c77d46066dd5f498ffae587ce0ccc0eca0
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-24 12:16:24 +1000

    model registration Habit

--- habits/admin.py
+++ habits/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Habit
 
-admin.site.register(Habit)
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('action', 'user', 'is_pleasant', 'is_public', 'frequency', 'time')
+    list_filter = ('is_pleasant', 'is_public', 'frequency')
+    search_fields = ('action', 'place', 'reward')
