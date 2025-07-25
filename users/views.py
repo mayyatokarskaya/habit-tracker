2af19d55d9827259b81ca3ea59e605624412984f
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-25 11:17:27 +1000

    create views.py for registration and authorization

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('habit_list')
+
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
