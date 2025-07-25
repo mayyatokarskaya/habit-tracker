c4ee77e3cc77eb4f01d23e831169efa979f1b512
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-25 11:32:01 +1000

    creating registration and authorization templates

--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = 'habit_list'
+LOGOUT_REDIRECT_URL = 'login'
