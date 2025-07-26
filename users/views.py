06b89e55c238bdf9fd5b15417931431ea6e69e4e
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-26 11:19:29 +1000

    add docstrings

--- users/views.py
+++ users/views.py
@@ -7,19 +7,41 @@ from .forms import CustomUserCreationForm
 
 
 class RegisterView(CreateView):
+    """
+    Представление для регистрации нового пользователя.
+
+    Использует форму CustomUserCreationForm для создания пользователя с email.
+    После успешной регистрации выполняет автоматический вход пользователя
+    и перенаправляет на страницу логина.
+    """
     form_class = CustomUserCreationForm
     template_name = "users/register.html"
     success_url = reverse_lazy("login")
 
     def form_valid(self, form):
+        """
+        Обработка успешного заполнения формы регистрации.
+
+        Сохраняет пользователя, выполняет вход и перенаправляет на логин.
+        """
         user = form.save()
         login(self.request, user)
         return redirect("login")
 
 
 class CustomLoginView(LoginView):
+    """
+    Представление для авторизации пользователя.
+
+    Использует шаблон users/login.html.
+    """
     template_name = "users/login.html"
 
 
 class CustomLogoutView(LogoutView):
+    """
+    Представление для выхода пользователя из системы.
+
+    После выхода перенаправляет на страницу логина.
+    """
     next_page = reverse_lazy("login")
