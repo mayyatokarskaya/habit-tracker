4e07892af6796aa05d01f5f6ca015bcb00cbcce1
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-26 10:46:54 +1000

    changes in tasks.py

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,12 +1,12 @@
 from celery import shared_task
 from django.utils.timezone import localtime
 from datetime import date
-
-from habits.models import Habit
-
+from .models import Habit
+from utils.telegram import send_telegram_message  # <--- обязательно
 
 @shared_task
 def print_hello():
+    print(f"[{localtime()}] 👋 Привет из Celery!")
 
 @shared_task
 def send_daily_reminders():
@@ -16,3 +16,9 @@ def send_daily_reminders():
         print(
             f"[{localtime()}] Напоминание: {habit.user.email} – {habit.action} в {habit.time} ({habit.place})"
         )
+
+@shared_task
+def remind_habit(habit_id):
+    habit = Habit.objects.get(id=habit_id)
+    message = f"⏰ Напоминание: пора выполнить привычку <b>{habit.action}</b> в {habit.time} на {habit.place}"
+    send_telegram_message(message)
